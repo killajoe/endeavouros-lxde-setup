a0f1f91c561efbe7abfc97cbee6b744566cfcaa8
Johannes Kamprad <archlinux@kamprad.net>
2021-12-10 12:53:43 +0100

    Update set_once.sh

--- set_once.sh
+++ set_once.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dbus-launch dconf load / < ~/xed.dconf
-chmod -R +x ~/.config/i3/scripts
+chmod -R +x ~/set_once.sh
 sed -i 's|\(@sh ~/set_once.sh\)|# \1|' ~/.config/lxsession/LXDE/autostart
 rm ~/xed.dconf
 rm ~/set_once.sh
